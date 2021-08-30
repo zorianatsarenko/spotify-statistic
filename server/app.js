e45f03a43c0105e520c3d6031aa8904ece22716b
zoriana tsarenko <zorianatsarenko@gmail.com>
2021-08-30 18:21:57 +0300

    ready to upload

---
+++ server/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const SpotifyWebApi = require("spotify-web-api-node");
+require("dotenv").config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  console.log("refreshing");
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  });
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
+
+app.listen(8888, () => {
+  console.log("app is listening on port 8888");
+});
